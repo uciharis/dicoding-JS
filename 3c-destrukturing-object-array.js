0aecadbd5fd79ef51e39099dde68b4651131f2c5
uciharis <harisnop@gmail.com>
2022-11-20 00:14:21 +0700

    ok

--- 3c-destrukturing-object-array.js
+++ 3c-destrukturing-object-array.js
@@ -1,1 +1,13 @@
-//
+//objek dan array banyak digunakan utk mengelola data di JS
+//JSON merupakan format data plg populer yg digunakan utk
+//-transaksi data saat ini.
+//destructuring array and object adalah sintak utk mengeluarkan
+//- nilai dr array dan object ke dalam satuan terkecil
+//contoh destructuring :
+const profil={firstNama: "john",
+lastNama: "doe",
+age: 18};
+const firstNama = profil.firstNama;
+const lastNama = profil.lastNama;
+const age = profil.age;
+console.log(firstNama,lastNama,age);
