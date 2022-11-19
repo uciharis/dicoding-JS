69b5c81166ae6c3b77c5238ff2e4e4883466d2f1
the hengker <40335023+uciharis@users.noreply.github.com>
2022-11-19 23:21:48 +0700

    materi tentang destructuring

--- 3c-destrukturing-object-array.js
+++ 3c-destrukturing-object-array.js
@@ -1,1 +1,8 @@
-//
+/*array dan object plg bnyk digunakan
+untuk mengelola data di JS. JSON adalah format
+data plg populer yang digunakan saat transaksi data.
+utk mengelola data tsb, ES6 menambahkan fitur
+utk destructuring object and array.
+
+destructuring merupakan
+ */
