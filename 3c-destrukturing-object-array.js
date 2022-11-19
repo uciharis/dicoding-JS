af3fa76654a8351fa553a265663458dad36b8b59
uciharis <harisnop@gmail.com>
2022-11-20 00:25:18 +0700

    contoh destructuring

--- 3c-destrukturing-object-array.js
+++ 3c-destrukturing-object-array.js
@@ -1,1 +1,19 @@
-//tees 123
+/* aray dan objek banyak digunakan
+untuk mengelola data di JS.
+JSON merupakan format data paling
+populer yang digunakan dalam transaksi data tsb
+.
+ES6 menambahkan fitur berupa destructuring object n array
+yang digunakan utk mengeluarkan nilai array dan objek
+ke dalam satuan yg lebih kecil.
+*/
+//contoh destructuring :
+const profil={
+    namaDepan: "joko",
+    namaBelakang:"kowi",
+    umur: 21
+};
+const namaDepan=profil.namaDepan;
+const namaBelakang=profil.namaBelakang;
+const umur=profil.umur;
+console.log(namaDepan,namaBelakang,umur);
