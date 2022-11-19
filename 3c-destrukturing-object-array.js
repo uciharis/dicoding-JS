fe2fd161c28fdff7c0964150376e091f1fbf9933
the hengker <40335023+uciharis@users.noreply.github.com>
2022-11-19 23:24:31 +0700

    contoh destruct

--- 3c-destrukturing-object-array.js
+++ 3c-destrukturing-object-array.js
@@ -4,5 +4,16 @@ data plg populer yang digunakan saat transaksi data.
 utk mengelola data tsb, ES6 menambahkan fitur
 utk destructuring object and array.
 
-destructuring merupakan
+destructuring merupakan sintak tuk mengeluarkan nilai
+array dan object ke dalam satuan yg lebih kecil.
  */
+//berikut contoh destructuring
+const profile= {
+    namaDepan: "jon",
+    namaBelakang: "doe",
+    umur: 67
+};
+const namaDepan= profile.namaDepan;
+const namaBelakang= profile.namaBelakang;
+const umur= profile.umur;
+console.log(namaDepan,namaBelakang,umur);
