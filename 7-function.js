61b13a21b16ef1016b2604fd4c75fc3cde46c0cc
uciharis <harisnop@gmail.com>
2022-12-23 12:13:22 +0700

    nambah penjelasan default parameter

--- 7-function.js
+++ 7-function.js
@@ -62,3 +62,42 @@ const perkalian= function(a,b){
     return a*b;
 }
 console.log(perkalian(3,4));
+
+//definisi
+// parameter vs argument
+//parameter : variabel yg didefinisikan sbg inputan dlm fungsi
+/*contoh: function nambah(a,b){
+    return a+b;
+}
+a dan b disebut parameter
+*/
+//sedangkan argumen adalah isian atau nilai yg dimasukkan kedalam parameter
+//contoh: nambah(4,9);
+//--------- ----------------
+//jika parameternya adalah objek maka bisa dimanfaatkan utk destructuring objek
+const pakai = {
+    id: 24,
+    displayName: "jokoh",
+    lengkapName: "mul jokoh"
+};
+function kenal({displayName, lengkapName}){
+    console.log(`${displayName} adalah ${lengkapName}`);
+}
+kenal(pakai);
+
+// --- default parameter ---
+//fungsi pada JS tidak melakukan pengecekan thd jumlah maupun tipe argumen yg masuk k dalam parameter
+//shg kita bs memasukkan argumen apapun meski tdk sesuai dg parameter yg kita inginkan
+//contoh: 
+function eksponenForm(basenumber, eksponen){
+    let result=basenumber**eksponen;
+    console.log(`${basenumber} pangkat ${eksponen} adalah ${result}`);
+}
+eksponenForm(2); //isinya Nan karena argumennya cuma 1 
+//maka dari itu perlu digunakan nilai default pada parameternya
+
+function eksponenFormFix(basenumber1, eksponen1=1){
+    let result1=basenumber1**eksponen1;
+    console.log(`${basenumber1} pangkat ${eksponen1} adalah ${result1}`);
+}
+eksponenFormFix(2); // aman, tidak lengkap isinya pun sudah ada default = 1
