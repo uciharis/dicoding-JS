805ebf46e1fcfe1abc6570e402bbdfba9821cfd7
uciharis <harisnop@gmail.com>
2022-12-25 13:42:18 +0700

    paragraf 1

--- 10-OOP.js
+++ 10-OOP.js
@@ -1,1 +1,11 @@
 // --- mengenal OOP ---
+//beberapa hal yg akan dibahas:
+/*
+1. pengertian paradigma
+2. constuctor function dan sintak class
+3. properti dan method dlm class
+4. pewarisan
+5. constructor dan method overriding
+6.object composition
+7. buult-in class
+*/
