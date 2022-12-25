56796d189dc9bb25e20c1a08796482740c256f9b
uciharis <harisnop@gmail.com>
2022-12-25 13:55:30 +0700

    pengantar OOP

--- 10-OOP.js
+++ 10-OOP.js
@@ -9,3 +9,33 @@
 6.object composition
 7. buult-in class
 */
+
+//objek dlm OOP dianggap seperti objek d dunia nyata
+//contoh: 
+//objek : mobil; propertis:merk, warna, tipe; methods: maju,mundur,belok
+
+//contoh kode OOP sederhana
+const mobil = {
+    //properties
+    merk: "honda",
+    warna: "ireng",
+    maxSpeed: 180,
+    //methods
+    drive: ()=>{
+        console.log("driving nih ..");
+    },
+    reverse: ()=>{
+        console.log("mundur banh ..");
+    },
+    belok:()=>{
+        console.log("belok cuy ..");
+    }
+}
+console.log(mobil.merk); //memanggil properti merk
+console.log(mobil.warna); //memanggil properti warna
+mobil.reverse(); //manggil metod
+
+//utk membuat salinan objek, gunakan fitur object blueprint
+//kita bs membuat cetakan objek dg macam properti dan metod
+//shg kita bs pake cetakan yg sama dg nilai properti dan metod yg lain
+//object blue print itu dinamakan class
