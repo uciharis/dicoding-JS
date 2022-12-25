3cbc9aef1ba474d4095e2469c46075bc98e8bd5e
uciharis <harisnop@gmail.com>
2022-12-25 13:59:40 +0700

    rename materi no.10 dan penambahan materi funct constructor

---
+++ 10b-OOP-fungsiKonstruksi.js
@@ -0,0 +1,4 @@
+//kebanyakan paradigma OOP pada bahasa class based language
+//-seperti java, c++ dan c#. 
+//JS bukanlah bahasa berbasis class setidaknya sblm adanya ES6.
+//namun beberapa konsep OOP dr bahasa lain dapat diterapkan di JS
