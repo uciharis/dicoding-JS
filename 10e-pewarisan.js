eb70be848289d39c64354b862c4cc60451cdf735
uciharis <harisnop@gmail.com>
2023-01-15 23:59:53 +0700

    teknik pewarisan

--- 10e-pewarisan.js
+++ 10e-pewarisan.js
@@ -88,4 +88,62 @@ email.sendBroadcastMessage(); // ??
 //dg konsep in, diwariskan sifat sifat yang berada di dalam sebuah class ke class lain.
 // konsep ini cocok ketika kita ingin membuat objek mirip dan memiliki sedikit perbedaan seperti kasus yang kita hadapi
 // implementasinya, kita tampung seluruh sifat yang sama pada sebuah class induk (superclass)
-//
+// -dan sifatnya nanti akan diwariskan kepada class di bawahnya (subclass)
+// kemudian pada subclass kita tambahkan kemampuan lain yg lebih spesifik
+// contoh, kita buat superclass bernama MailService yang mengandung
+// seluruh sifat yang sama pada WhatsAppService dan EmailService
+// contoh superclass
+class MailService {
+    constructor (sender) {
+        this.sender = sender;
+    }
+    sendMessage(message, receiver){
+        console.log(`${this.sender} sent ${message} to ${receiver}`);
+    }
+}
+
+//contoh subclass 1
+class WhatsAppService extends MailService {
+
+}
+//contoh subclass 2
+class EmailService extends MailService {
+
+}
+
+//di dalam masing2 subclass, kita dapat mendefinisikan metod yang
+//spesifik seperti sendBroadcastMessage() untuk WhatsAppService dan
+// -sendDelayedMessage() untuk EmailService
+
+// subclass 1
+class WhatsAppService extends MailService {
+    sendBroadcastMessage(message, receivers) {
+        for (const receiver of receivers) {
+            this.sendMessage(message. receiver);
+        }
+    }
+}
+
+// subclass 2
+class EmailService extends MailService (
+    sendDelayedMessage(message, receiver,delay){
+        setTimeout(()=>{
+            this.sendMessage(message, receiver);
+        }, delay);
+    }
+)
+//lihat bahwa di dalam subclass WhatsAppService dan EmailService kita tetap
+// memiliki akses terhadap method dari superclass melalui keyword this
+// karena subclass mewarisi sifat dari superclass.
+// dengan teknik pewarisan seperti ini, kita dpat membuat objek serupa
+// dengan cara yang jauh lebih efektif.
+
+const whatsapp= new WhatsappService('+6281234567890');
+const email = new EmailService('dimas@dicoding.com');
+whatsapp.sendMessage('hello', '+6289876543210');
+whatsapp.sendBroadcastMessage('hello', ['+6289876543210','+6282234567890 ']);
+whatsapp.sendDelayedMessage(); //error
+
+email.sendMessage('hello', 'john@doe.com');
+email.sendDelayedMessage('hello', 'john@doe.com', 3000);
+email.sendBroadcastMessage(); //error
