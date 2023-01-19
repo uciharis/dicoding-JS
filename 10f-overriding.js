2d15a406cf60a133335c3bd51ab984ab7f56859f
uciharis <harisnop@gmail.com>
2023-01-19 23:50:46 +0700

    method overriding

--- 10f-overriding.js
+++ 10f-overriding.js
@@ -50,3 +50,96 @@ class WhatsAppService extends MailService {
 // pada sebuah class. selain itu, metod constructor didefinisikan dengan perubahan sesuai kebutuhan kita.
 // misal menambahkan properti dan argumen isBusinnes. satu hal yang sangat penting dalam menerapkan
 // constructor overriding adalah jangan sampai lupa memanggil metod super()
+// memanggil method super() artinya memanggil constructor superclass, yaitu
+// MailService. dengan begitu kita tidak perlu mendefinisikan dan menetapkan nilai properti umum satu per satu
+// meskipun dalam contoh kali ini properti yang bersifat umum hanya ada satu, yaitu sender,
+// tetapi jika jumlah propertinya bertambah, kita tidak perlu menulis kode yang berulang pada setiap subclass
+// pada sintak class, pemanggilan super() tidak boleh terlewatkan karena akan menyebabkan ReferenceError
+
+class MailService {
+    constructor(sender){
+        this.sender = sender;
+    }
+}
+class WhatsAppService extends MailService{
+    constructor(sender, isBussines){
+        this.sender=sender;
+        this.isBusinnes=isBussines;
+    }
+}
+const whatsapp = new WhatsAppService("+628123456789", true); // output ReferenceError: must call super constructor in derived class before accesing "this" or returining from derived constructor
+
+// --- method overriding ---
+// konsep overriding juga bisa diterapkan pada method class.
+// method overriding biasanya dilakukan ketika kita ingin mengubah implementasi method warisan
+// superclass. contoh pada class MailService terdapat method send() yang sudah didefinisikan implementasinya.
+// namun jika pada subclass WhatsAppService kita membutuhkan implementasi yang berbeda, kita bisa
+// override methode send()
+// cara override method pada subclass mirip seperti override constructor, tetapi kita tidak perlu memanggil method super di dalamnya
+
+class MailService{
+    constructor(sender){
+        this.sender= sender;
+    }
+    sendMessage(message, receiver) {
+    console.log(`${this.sender} sent ${message} to ${receiver}`);
+}
+}
+class WhatsAppService extends MailService{
+    constructor(sender, isBusinnes){
+        super(sender);
+        this.isBusinnes=isBusinnes;
+    }
+    // overriding method
+    sendMessage(message, receiver){
+        console.log(`${this.sender} sent ${message} to ${receiver} via whatsapp`);
+    }
+}
+const mailService = new MailService("some sender");
+const whatsappService = new WhatsAppService("+6281234567890", true);
+mailService.sendMessage("hai apa kabar?", 'someReceiver');
+whatsappService.sendMessage("hai apakabar?", "+68289876543210");
+// output service : 
+/**
+ * someSender sent Hai, apa kabar? to someReceiver
+ * +6281234567890 sent Hai, apa kabar? to +6289876543210 via Whatsapp
+ */
+// jika dibutuhkan, kita juga bisa memanggil method sendMessage() dari superclass
+// melalui keyword super.sendMessage().
+class MailService {
+    constructor(sender) {
+    this.sender = sender;
+    }
+    sendMessage(message, receiver) {
+    console.log(`${this.sender} sent ${message} to ${receiver}`);
+    }
+
+}  
+class WhatsAppService extends MailService {
+    constructor(sender, isBusiness) {
+    super(sender);
+    this.isBusiness = isBusiness;
+    }
+    
+    // Overriding method
+    sendMessage(message, receiver) {
+      // memanggil method sendMessage pada superclass
+    super.sendMessage(message, receiver);
+
+    console.log('message sent via WhatsApp');
+    }
+
+
+
+const mailService = new MailService('someSender');
+const whatsappService = new WhatsAppService('+6281234567890', true);
+
+mailService.sendMessage('Hai, apa kabar?', 'someReceiver');
+whatsappService.sendMessage('Hai, apa kabar?', '+6289876543210');
+
+/**
+  * Output:
+  * someSender sent Hai, apa kabar? to someReceiver
+  * +6281234567890 sent Hai, apa kabar? to +6289876543210
+  * message sent via WhatsApp
+  */
