06ccc2986405cab330b7021eb85b3b5625cf1b28
uciharis <harisnop@gmail.com>
2023-02-09 20:31:58 +0700

    pengenalan modul pada node.js

--- CoffeeMachine/index.js
+++ CoffeeMachine/index.js
@@ -1,3 +1,12 @@
+/** --- membuat Projek Javascript ---
+ * cara --> buat folder dg nama CoffeMachine, lalu ketik perintah d terminal
+ * npm init
+ * 
+ * menggunakan npm init adalah pendekatan terbaik ketimbang bikin package.json secara manual
+ * 
+ * 
+ */
+
 console.log('menyalakan mesin kopi');
 console.log("Menggiling biji kopi");
 console.log("Memanaskan air");
@@ -5,3 +14,27 @@ console.log("Mencampurkan air dan kopi");
 console.log("Menuangkan kopi ke dalam gelas");
 console.log("Menuangkan susu ke dalam gelas");
 console.log("Kopi Anda sudah siap!");
+// jalankan dengan ketik " node index.js" di folder CoffeeMachine
+// pada berkas package.json terdapat beberapa objek penting salah satunya adalah object script
+// seperti ini : (ada d dalam berkas package.json)
+/*
+    "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+    },
+  */
+ //secara default objek tsb akan terbentuk ketika package.json dibuat menggunakan perintah npm init
+// objek script adalah objek yang mengandung kumpulan script di dalamnya.
+// utk menjalankan script tsb, gunakan nmp run <script-name>
+/**
+ * npm run test
+ * test adalah objek di dalam scripts yang berisi echo blablabla...
+ * 
+ */
+// dg menjalankan script tes , artinya kita mengeksekusi kode yang berada di dalam nilai test
+// objek script biasanya utk menetapkan script yang sering digunakan secara berkala
+// seperti menjalankan aplikasi, compiling source code ke tahap produksi dan melakukan testing
+// utk menetapkan nilai baru pada objek script, kita tuliskan nama script sebagai properti
+// dan kemudian tuliskan perintah yng dieksekusi sbg nilai dari properti tsb.
+// mari kita buat script baru utk menjalankan kode dari berkas index.js
+// beri nama properti "start" dg nilai perintah "node index.js"
+// lalu jalankan di terminal node index.js (jgn lupa tutup file nya tsb)
---
+++ CoffeeMachine/state.js
@@ -0,0 +1,9 @@
+/** --- pengenalan module ----
+ * ketika aplikasi sudah berkembang, kita bs saja menuliskan kode hanya dalam satu berkas
+ * namun bayangkan apabila kita harus mencari baris kode yang memiliki bug dari jutaan kode baris
+ * untuk itulah pentingnya membagi projek menjadi beberapa berkas berdasarkan tugas dan fungsinya
+ * disitulah kita membuat sebuah modul JS. tak lain adlah utkmenghubungkan berkas JS yang terpisah agar slg dapat digunakan
+ * 
+ * 
+ * 
+ */
