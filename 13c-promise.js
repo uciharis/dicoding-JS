b4c359705bc615ff35a0b014cb4212e2546c3ba7
uciharis <harisnop@gmail.com>
2023-02-18 21:59:10 +0700

    materi promise

---
+++ 13c-promise.js
@@ -0,0 +1,16 @@
+/** -- promise --
+ * merupakan fitur penting es6
+ * promise menggantikan peran callback dg ciri khas pake funct .then
+ * namun mengapa fitur ini disebut promise/ janji ?
+ * ternyata fungsi tsb memang sesuai dg namanya, yaitu berjanji, seolah olah fungsi seperti berjanji
+ * 
+ * analoginya sbb:
+ * ketika kita pesan kopi, pelayan secara tdk langsung berjanji kepada kita utk membuatkan kopi dan
+ * mengantarkanny kepada kita. namun janji bs hanya tinggal janji, artinya tidak terlaksana atau terpenuhi
+ * maka dari itu promise memiliki 3 kondisi yaitu :
+ * -pending (janji dlm proses)
+ * -fulfilled (janji terpenuhi)
+ * -rejected (janji gagal)
+ */
+
+// ---membuat promise ---
