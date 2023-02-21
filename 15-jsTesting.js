0e7e642d48209288c1a4aa535c9f1a61a200df09
uciharis <harisnop@gmail.com>
2023-02-21 23:10:06 +0700

    menulis kode pengujian

--- 15-jsTesting.js
+++ 15-jsTesting.js
@@ -7,3 +7,55 @@
 
 // dalam pengujian kode, kita menggunakan library tambahan utk mempermdah penulisan pengujian kode
 // jest adalah salah satu framework testing yg terkenal utk menuliskan kode pengujian pada JS
+
+// perhatikan kode berikut ini
+// ini adalah unit test jest
+// format :
+/** --- kode framework jest ---
+ * 
+ * ini formatnya :
+ * 
+ * test('deskripsi test mu', ()=> {
+ * expect(perintah).matcher(nilai yg diekspektasikan);
+ * });
+ * 
+ * contoh penulisan :
+ * 
+ * test('kode penjumlahan', ()=> {
+ *  expect(2+2).toBe(4);
+ * });
+ * --- akhir kode ---
+ * 
+ * toBe(4) : disebut sbg matcher
+ * terdapat beberapa pilihan matcher yaitu :
+ * 
+ *      toBe(x) : nilai yg diharapkan adalah x
+ *      toEqual(x) : nilai yg diharapkan sama
+ *      toBeNull() : nilai yg diharapkan 0
+ *      toBeTruthy() : nilai yg diharapkan truth
+ *      toBeFalsy() : nilai yg diharapkan false
+ * 
+ * info matcher yang lain : https://jestjs.io/docs/using-matchers
+ * 
+ */
+
+// pada framework jest, kita juga bs membuat test secara grup berdasar karakteristik atau grup yg sama
+// pake : describe
+
+/** --- penggunaan describe ---
+ * 
+ * --- ini kode ---
+ * 
+ * describe('pengujian aritmatika', ()=> {
+ *      test1('#dua tambah dua adalah 4', ()=> {
+ *       expect(2+2).toBe(4);
+ * });
+ * test2('#dua dikali tiga adalah enam', ()=> {
+ * expect(2*3).toBe(6)});
+ * });
+ * });
+ * 
+ * 
+ * 
+ * 
+ */
