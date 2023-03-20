3a94b493a8e9dab53ea3ca1b0d67eae410ace7b8
uciharis <harisnop@gmail.com>
2023-03-20 09:53:49 +0700

    nodejs sudah terupdate ke latest dg nvm, perbaikan pada server.js linked pada bab  membuat httpserver

--- hapi-web-server/server.js
+++ hapi-web-server/server.js
@@ -1,16 +1,12 @@
 const Hapi = require('@hapi/hapi');
 
-const init = async()=> {
-    const server = Hapi.server({
-        port : 5000,
-        host : 'localhost'
-
-    });
-    await server.start();
-    console.log('server berjalan di',server.info.uri);
-};
-ProcessingInstruction.on('unhandledRejection',(err)=>{
-    console.log(err);
-    Process.exit(1);
+const init = async ()=>{
+const server = Hapi.server({
+port: 5000,
+host: 'localhost'
 });
+await server.start();
+console.log(`server berjalan pada ${server.info.uri}`);
+}
+
 init();
