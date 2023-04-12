a781099539c9dfe6fed1a756c62932bcdb90ba13
uciharis <harisnop@gmail.com>
2023-04-12 23:22:18 +0700

    materi CORS

---
+++ 18d-sameOriginPolicy.js
@@ -0,0 +1,8 @@
+// --- Same-Origin Policy ---
+
+// server dapat tampung sebuah web, aplikasi, gambar, video dll
+// ketika server menampung wbm mungkin beberapa dta gambar, video, stylesheet biasanya diambil dari
+// alamat server lain atau origin berbeda
+// contoh stylesheet yang diambil dari Bootstrap CDN atau gambar dari server Unsplash
+// hal ini wajar dilakukan
+
