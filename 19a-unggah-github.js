7b53dcf000a31e7806a6aaf9533807ec219d7c0c
the hengker <40335023+uciharis@users.noreply.github.com>
2023-05-13 22:05:46 +0700

    Create 19a-unggah-github.js
    belajar versioning git dan github (bersambung)

---
+++ 19a-unggah-github.js
@@ -0,0 +1,8 @@
+// --- Mengunggah Proyek Web Server ke Github ---
+
+// kita akan memanfaatkan teknologi git dan github.
+
+// git adala tools manajemen source kode atau dikenal dg istilah Versioning
+// lewat git kita dapat mentrack perubahan kode, siapa yang merubahnya, hingga melakukan branching
+// selengkapnya tentang git dan github : https://www.dicoding.com/academies/317
+// git berjalan di local , maka agar dapat diakses dari mana saja maka menggunakan github
