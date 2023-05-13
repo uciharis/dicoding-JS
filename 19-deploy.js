0f4384ed79fc000f4775ff8599c321a92ad5d8ba
the hengker <40335023+uciharis@users.noreply.github.com>
2023-05-13 21:59:37 +0700

    Update 19-deploy.js
    memilih dan mengatur instance EC2 Amazon (selesai)

--- 19-deploy.js
+++ 19-deploy.js
@@ -38,4 +38,49 @@ jika instance tidak dipakai maka tidak ada biayanya.
 // pilih IAM
 // pilih user
 // klik Add User utk bkin user dengan level lebih rendah dari root
-// 
+// isi data seperti berikut:
+// username : diisi sesuai keingininan
+// access type : AWS Management Console Access
+// console password : isi sesuai keinginan
+// require password reset : uncheck
+// create group lalu tambahkan user sesuai keinginan
+
+// bikin dan jalankan Amazon EC2 Instance
+
+// pembahasan sebelumnya adalah membuat akun IAM yang punya akses penuh ke service Amazon EC2
+// utk membuat instance, akses menu all services -> compute -> EC2
+// atau melalui search bar ketikkan EC2
+// lalu klik "launch instance"
+// isikan nama dari web server,
+// pilih OS image
+// pilih instance type : "t2.micro"
+// key pair (login) klik create new key pair
+// isikan key pair name : " notes-api-webserver"
+// pastikan berkas "notes-api-webserver.pem" terunduh
+// simpan file tsb utk mengoperasikan instance lewat SSH
+// sesuaikan network setting :
+// klik edit
+// security group name diisi app-server-sg
+// description: allow custom TCP port 5000
+// klik Add security group rule
+// isikan dengan detail berikut:
+// - Type : Custom TCP
+// - port range : 5000
+// - Source Type : Anywhere
+// - description : Application Port
+
+// lalu klik launch
+
+
+// Mengoperaskan EC2 instance lewat SSH
+
+// pada instance yang dibuat tadi, klik "connect",
+// lalu pilih tab SSH client
+// copy command di example, 
+// buka terminal di folder berisi file "notes-api-webserver.pem" dan paste command tadi
+// lalu akan muncul warning 
+// solusinya adalah mengubah permission berkas "notes-api-webserver.pem" dengan perintah berikut
+// chmod 400 notes-api-webserver.pem
+// lalu lulangi command di example
+// instance tsb sudah berhasil terhubung lewat SSH dan hanya bisa dioperasikan dengan command line
+// berikut beberapa perintah linux sederhana : https://ubuntu.com/tutorials/command-line-for-beginners#1-overview
