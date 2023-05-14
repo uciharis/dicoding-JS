1abb1bbf5d36b17db776742d35c1fd18dccd0457
the hengker <40335023+uciharis@users.noreply.github.com>
2023-05-14 14:04:41 +0700

    Create 20-consumeAPI-tes-postman.js
    pengantar Penggunaan POSTman

---
+++ 20-consumeAPI-tes-postman.js
@@ -0,0 +1,10 @@
+// --- Consume dan Uji RESTful API pake POSTMAN
+
+// materi sebelumnya, kita uji webserver atau RESTful API pake aplikasi client yang dibuat dicoding
+// nyatanya pada kebanyakan pengembangan API, jarang ada yang menyediakan front-end client utk uji fungsionalitas API
+// utk itu digunakan POSTMAN utk menguji API karna punya API caller dalam HTTP req
+// API punya GUI sehingga mudah dipahami
+// kita tidak perlu berhadapan dg kode rumit hanya utk request HTTP seperti pake cURL
+// silahkan install postman lewat web nya : https://www.postman.com/downloads/
+
+// Konsumsi API pake Postman
