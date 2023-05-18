06a22031a9dde611fe1f3234b33721f7422315b3
the hengker <40335023+uciharis@users.noreply.github.com>
2023-05-18 18:32:02 +0700

    Create 20a-automate-use-postman.js
    otomatisasi pake postman dg postman collection dan environment

---
+++ 20a-automate-use-postman.js
@@ -0,0 +1,14 @@
+// --- Otomatisasi Testing pake POSTman
+
+// Postman ternyata bisa melakukan uji secara otomatis sehngga tak perlu
+// lagi melihat respon dari server secara manual utk memastikan respon sesuai harapan
+// pengujian otomatis di POSTman menggunakan js, serupa dg unit tes dan integration tes
+
+// melalui testing oto,kita bs menguji nilai dari status kode, propert header , hingga body respons.
+// pengujian akan "pass" ketika semua variabel yang diuji sesuai ekspektasi
+// bila tidak maka akan failed
+
+// kita akan berkenala dg fitur collection dan environment yang ada di POSTman
+// fitur ini akan dipakai dalam pengujian
+
+// --- postman collection
